df6b9597b70ba6df581b9d0bdb08e0c1d60d552b
Dafisto <chrisreid2002@gmail.com>
2024-04-21 11:24:26 -0600

    final update to css and js

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,36 +3,115 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 var costLabel = document.getElementById("calculated-cost");
-var halfDay = document.getElementById("half");
-var fullDay = document.getElementById("full");
 var clearButton = document.getElementById("clear-button");
-var dayOfWeek = document.getElementById("monday","tuesday","wednesday","thursday","friday");
+var mondayButton = document.getElementById("monday")
+var tuesdayButton = document.getElementById("tuesday")
+var wednesdayButton = document.getElementById("wednesday")
+var thursdsayButton = document.getElementById("thursday")
+var fridayButton = document.getElementById("friday")
+var dayCounter = 0
+var dailyRate = 40
+let fullDay = document.getElementById("full")
+let halfDay = document.getElementById("half")
 
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-function daySelection(){
-    if (dayOfWeek.classList.contains("clicked")) {
-        dayOfWeek.classList.remove("clicked");
+function mondayClicked() {
+    if (mondayButton.classList.contains("clicked")) {
+        mondayButton.classList.remove("clicked")
+        dayCounter -= 1
     }
     else {
-        dayOfWeek.classList.add("clicked");
+        mondayButton.classList.add("clicked")
+        dayCounter += 1
     }
-    calculatedCost();
+    calculatedCost()
 }
 
-dayOfWeek.addEventListener("click", daySelection);
+mondayButton.addEventListener("click", mondayClicked)
+
+function tuesdayClicked() {
+    if (tuesdayButton.classList.contains("clicked")) {
+        tuesdayButton.classList.remove("clicked")
+        dayCounter -= 1
+    }
+    else {
+        tuesdayButton.classList.add("clicked")
+        dayCounter += 1
+    }
+    calculatedCost()
+}
+
+tuesdayButton.addEventListener("click", tuesdayClicked)
+
+function wednesdayClicked() {
+    if (wednesdayButton.classList.contains("clicked")) {
+        wednesdayButton.classList.remove("clicked")
+        dayCounter -= 1
+    }
+    else {
+        wednesdayButton.classList.add("clicked")
+        dayCounter += 1
+    }
+    calculatedCost()
+}
+
+wednesdayButton.addEventListener("click", wednesdayClicked)
+
+function thursdayClicked() {
+    if (thursdsayButton.classList.contains("clicked")) {
+        thursdsayButton.classList.remove("clicked")
+        dayCounter -= 1
+    }
+    else {
+        thursdsayButton.classList.add("clicked")
+        dayCounter += 1
+    }
+    calculatedCost()
+}
+
+thursdsayButton.addEventListener("click", thursdayClicked)
+
+function fridayClicked() {
+    if (fridayButton.classList.contains("clicked")) {
+        fridayButton.classList.remove("clicked")
+        dayCounter -= 1
+    }
+    else {
+        fridayButton.classList.add("clicked")
+        dayCounter += 1
+    }
+    calculatedCost()
+}
+
+fridayButton.addEventListener("click", fridayClicked)
+
+
+
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
 function clearAll() {
-    if (dayOfWeek.classList.contains("clicked"));
-     {
-        dayOfWeek.classList.remove("clicked");
+    if (mondayButton.classList.contains("clicked")) {
+        mondayButton.classList.remove("clicked")
     }
+    if (tuesdayButton.classList.contains("clicked")) {
+        tuesdayButton.classList.remove("clicked")
+    }
+    if (wednesdayButton.classList.contains("clicked")) {
+        wednesdayButton.classList.remove("clicked")
+    }
+    if (thursdsayButton.classList.contains("clicked")) {
+        thursdsayButton.classList.remove("clicked")
+    }
+    if (fridayButton.classList.contains("clicked")) {
+        fridayButton.classList.remove("clicked")
+    }
+    dayCounter = 0
     calculatedCost();
 }
 
@@ -41,49 +120,38 @@ clearButton.addEventListener("click", clearAll);
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $25, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-function halfDayClick() {
-    var day = document.getElementById("half");
-    if (day.classList.contains("clicked") != true) {
+
+function halfDayClick() {     
+    if (fullDay.classList.contains("clicked") == true) {
         fullDay.classList.remove("clicked");
-        // day = "half";
-        day.classList.add("clicked");       
+        halfDay.classList.add("clicked");        
+        dailyRate = 25        
+        }
     calculatedCost();
 }
-}
+
 halfDay.addEventListener("click", halfDayClick);
 
 
 // when the full-day button is clicked, the daily rate is set back to $40, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-function fullDayClick() {
-    var day = document.getElementById("full");
-    if (day.classList.contains("clicked") != true) {
-        halfDay.classList.remove("clicked");
-        // day = "full";
-        day.classList.add("clicked"); 
+
+function fullDayClick() {    
+    if (halfDay.classList.contains("clicked") == true) {
+        fullDay.classList.add("clicked");
+        halfDay.classList.remove("clicked");         
+        dailyRate = 40;   
+        }    
     calculatedCost();
 }
-}
+
 fullDay.addEventListener("click", fullDayClick);
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-function dayCounterCheck() {    
-    for (numOfDays in dayOfWeek) {
-        if (dayOfWeek.classList.contains("clicked")) {
-            numOfDays += 1;
-        }
-    }
-}
 
 function calculatedCost() {
     let costLabel = document.getElementById("calculated-cost");
-    var totalCost = 0;
-    if (halfDay.classList.contains("checked")) {
-        totalCost = dayCounterCheck() * 25;       
-    }
-    else {
-        totalCost = dayCounterCheck() * 40;        
-    }
+    totalCost = dayCounter * dailyRate;             
     costLabel.innerHTML = totalCost;
 }
